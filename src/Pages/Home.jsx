46c399bb0080ef945e79237446db534a90570c22
Hlobisile Lukhele <n.thwala03@gmail.com>
2025-10-06 15:33:23 +0200

    imported and rendered the component on the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import Hero from '../components/Hero.jsx';
+import ServicesCards from '../components/ServicesCards.jsx';
 
 function Home() {
   return (
     <div>
       <Hero />
+      <ServicesCards />
     </div>
   );
 }
-
 export default Home;
