7521e6ac8a954dec3b48c28793730583745640c9
Hlobisile Lukhele <n.thwala03@gmail.com>
2025-10-20 09:30:49 +0200

    Add FAQs component to Contact page for enhanced user support

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactForm from "../components/ContactForm.jsx"
+import FAQs from "../components/FAQs.jsx"
 
 function Contact() {
   return (
     <div>
       <h1 className='flex items-center justify-center text-3xl font-bold p-8'>Contact Us</h1>
       <ContactForm />
+      <FAQs />
     </div>
   );
 }
