6f803bf9b4b1cebff1eff6845d85c39655c25dab
Hlobisile Lukhele <n.thwala03@gmail.com>
2025-10-20 09:30:25 +0200

    Add Pest Control route to App component for service navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './shared/Footer';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import PestControl from './Pages/Services/pest-control';
+//import HygieneControl from './Pages/Services/HygieneControl';
 
 
 function App() {
@@ -15,6 +17,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        {/* Pest Control route */}
+        <Route path="Pages/Services/pest-control" element={<PestControl />} />
+        {/* <Route path="/services/hygiene-control" element={<HygieneControl />} /> */}
+        
       </Routes>
       <Footer />
     </>
