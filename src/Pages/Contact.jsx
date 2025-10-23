b11088405be018edc48df5cbb36d532ed60762c2
Hlobisile Lukhele <n.thwala03@gmail.com>
2025-10-23 13:57:53 +0200

    Add text color to Contact Us header for branding consistency

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ import FAQs from "../components/FAQs.jsx"
 function Contact() {
   return (
     <div>
-      <h1 className='flex items-center justify-center text-3xl font-bold p-8'>Contact Us</h1>
+      <h1 className='flex items-center justify-center text-3xl font-bold p-8 text-[#013c15]'>Contact Us</h1>
       <ContactForm />
       <FAQs />
     </div>
